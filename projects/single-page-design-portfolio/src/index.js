9e72790110d9cb37a092464362060b802845de95
Alim Nagoev <nagoev@MacBook-Pro-Alim.local>
2023-01-18 15:36:20 +0300

    Add Single-page design portfolio

--- projects/single-page-design-portfolio/src/index.js
+++ projects/single-page-design-portfolio/src/index.js
@@ -1,92 +1,40 @@
-// 🚀 Images
-// import team from '/assets/images/icon-team-builder.svg';
-
-// 🚀 Styles
+import Swiper, { Navigation, A11y } from 'swiper';
 import './style.css';
 
-// 🚀 Render Skeleton
-const root = document.querySelector('#app');
-root.innerHTML = `
- <div class='page'>
-  <header class='header'>
-    <nav class='nav'>
-      <a href='' class='logo'>
-        <img src='' alt='' class='logo__image'>
-      </a>
-      <button class='button'>Free Consultation</button>
-    </nav>
-  </header>
+import 'swiper/swiper.min.css';
+// import 'swiper/scss/navigation';
+// import 'swiper/scss/pagination';
 
-  <main class='main'>
-    <!--🚀 Intro-->
-    <section class='intro'>
-      <h1 class='intro__title'>Design solutions made easy</h1>
-      <p class='intro__lead'>With over ten years of experience in various design disciplines, I’m your one-stop shop for
-        your design needs.</p>
-      <ul class='intro__grid'>
-        <li>
-          <p>Graphic Design</p>
-        </li>
-        <li>
-          <p>UI/UX</p>
-        </li>
-        <li>
-          <p>Apps</p>
-        </li>
-        <li>
-          <p>Photography</p>
-        </li>
-        <li>
-          <p>Illustrations</p>
-        </li>
-        <li>
-          <p>Motion Graphics</p>
-        </li>
-      </ul>
-    </section>
-    <!--🚀 Person-->
-    <section class='person '>
-      <img src='' alt='' class='person__image'>
-      <div class='person__content'>
-        <h2>I’m Amy, and I’d love to work on your next project</h2>
-        <p>I love working with others to create beautiful design solutions. I’ve designed everything from brand
-          illustrations to complete mobile apps. I’m also handy with a camera!</p>
-        <button class='button'>Free Consultation</button>
-      </div>
-    </section>
-    <!--🚀 Portfolio-->
-    <section class='portfolio'>
-      <h2>My Work</h2>
-    </section>
-    <!--🚀 Contact Us-->
-    <section class='contact'>
-      <h2>Book a call with me</h2>
-      <p>I’d love to have a chat to see how I can help you. The best first step is for us to discuss your project during
-        a free consultation. Then we can move forward from there.</p>
-      <button class='button'>Free Consultation</button>
-    </section>
-  </main>
-
-  <footer class='footer'>
-    <nav class='nav'>
-      <a href='' class='logo'>
-        <img src='' alt='' class='logo__image'>
-      </a>
-      <button class='button'>Free Consultation</button>
-    </nav>
-  </footer>
-</div>
-`;
+// Init swiper
+const indexSwiper = new Swiper('.swiper', {
+  modules: [Navigation, A11y],
+  direction: 'horizontal',
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 15,
+  loop: true,
+  a11y: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    768: {
+      spaceBetween: 30,
+    },
+  },
+});
 
 // ==========================
 // 🚀 Query Selectors
 // ==========================
-const DOM = {};
-
 // ==========================
 // 🚀 Query Selectors
 // ==========================
-
 // ==========================
 // 🚀 Events
 // ==========================
